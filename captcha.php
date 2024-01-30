37619179a6f4201016d2b97c4ff6a0acaa5855bd
Felix Caffier <broozar@web.de>
2024-01-30 01:44:40 +0100

    initial release

---
+++ captcha.php
@@ -0,0 +1,28 @@
+<?php
+
+function checkCaptcha($url = "https://captcheck.netsyms.com/captcheck.min.js")
+{
+	if (empty($_POST['captcheck_session_code']))
+		return "";
+
+	$data = [
+		'session_id' => $_POST['captcheck_session_code'],
+		'answer_id' => $_POST['captcheck_selected_answer'],
+		'action' => "verify"
+	];
+	$options = [
+		'http' => [
+			'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+			'method' => 'POST',
+			'content' => http_build_query($data)
+		]
+	];
+	$context = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	$resp = json_decode($result, TRUE);
+	if (!$resp['result']) {
+		return "<div class='error'>CAPTCHA did not verify: " . $resp['msg'] . "</div>";
+	} else {
+		return "verified";
+	}
+}
---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+require("qrcode.php");
+require("captcha.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>QR Code Generator</title>
+	<meta charset="utf-8">
+	<style>
+        * {
+            font-family: Helvetica, sans-serif;
+            box-sizing: border-box;
+        }
+
+        html {
+            width: 100%;
+            min-height: 100vh;
+        }
+
+        body {
+            padding: 0;
+            margin: 0;
+            min-height: 100vh;
+            display: flex;
+        }
+
+        h1 {
+            font-size: 2em;
+            margin: 0 0 0.5em 0;
+            padding: 0;
+        }
+
+        #content {
+            margin: auto;
+            width: 50%;
+            min-width: 200px;
+            max-width: 600px;
+            padding: 1em;
+            background-color: #eeeeee;
+            border: #dddddd 1px solid;
+        }
+
+        .error {
+            display: block;
+            color: white;
+            background-color: #dd0000;
+            border: #cc0000 1px solid;
+            margin: 1em 0 1em 0;
+            padding: 0.5em;
+        }
+
+        .qrimage {
+            margin: 0 0 1em 0;
+        }
+
+        input[type=text] {
+            background-color: #fafafa;
+            border: 0;
+            padding: 10px 15px 10px 15px;
+            font-size: 1em;
+            width: 100%;
+            margin: 0 0 1em 0;
+        }
+
+        input[type=text]:hover {
+            background-color: white;
+            transition: background 0.2s;
+        }
+
+        input[type=submit] {
+            display: inline-block;
+
+            padding: 10px 15px 10px 15px;
+
+            font-size: 1em;
+            text-align: center;
+
+            color: #fff;
+            background-color: #646464;
+            border: 0;
+            margin: 1em 0 0 0;
+        }
+
+        input[type=submit]:hover {
+            background-color: #8c8c8c;
+            transition: background 0.2s;
+        }
+
+	</style>
+	<script src="https://vps.trisymphony.com/captcheck/captcheck.js"></script>
+</head>
+<body>
+<div id="content">
+	<h1>QR Code Generator</h1>
+
+	<?php
+	$captcha = checkCaptcha();
+	if (!empty($captcha)) {
+		if (strcmp($captcha, "verified") === 0) {
+			$generator = new WF_QRCode($_REQUEST['encodeURL'], $_REQUEST);
+			ob_start();
+			$image = $generator->render_image();
+			imagepng($image);
+			$png = ob_get_clean();
+			imagedestroy($image);
+			echo sprintf("<img src='data:image/png;base64,%s' alt='qrcode_image' class='qrimage'>", base64_encode($png));
+		} else {
+			echo $captcha;
+		}
+	}
+	?>
+
+	<form method="post">
+		<input type="text" name="encodeURL" placeholder="Target URL">
+		<input type="hidden" name="s" value="qr">
+		<div class="captcheck_container"></div>
+		<input type="submit">
+	</form>
+
+</div>
+</body>
+</html>
